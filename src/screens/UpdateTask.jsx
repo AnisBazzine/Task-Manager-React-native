2b49c5ad47bac47b2f554aff18d68f307f03015b
AnisBazzine <mohamedanis1002@gmail.com>
2024-01-13 11:30:42 +0100

    Create Screens

---
+++ src/screens/UpdateTask.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function UpdateTask() {
+  return (
+    <View>
+      <Text>UpdateTask</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
